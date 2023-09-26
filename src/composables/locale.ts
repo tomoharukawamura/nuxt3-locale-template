b10a14cfe25aafed27b41ec348b9b648fb9cd8ee
tomoharukawamura <tomoharu-uts1-23@g.ecc.u-tokyo.ac.jp>
2023-09-26 20:19:14 +0900

    localeリファクタ

--- src/composables/locale.ts
+++ src/composables/locale.ts
@@ -1,44 +1,30 @@
 import { createLink } from '~/utils/createLink'
 import localeData from '../assets/locale/index.json'
 export const useLocale = (route: Ref<string>) => {
-  const locale = useState<'ja' | 'en'>('locale', () => 'ja')
-
   const isTopPage = computed(() => route.value === '/' || route.value === '/en')
   const pageName = computed(() => isTopPage.value? 'index': route.value.split('/')[1])
-  const localeFromRoute = computed<'en' | 'ja'>(() => {
-    if(route.value.endsWith('/en')){
-      return 'en'
-    } else {
-      return 'ja'
-    }
-  })
-
-  if(process.server){
-    locale.value = localeFromRoute.value
-  }
+  const locale = computed<'en' | 'ja'>(() => route.value.endsWith('/en') ? 'en' : 'ja')
   
   const setLocale　= (l: 'ja' | 'en') => {
     if(l === locale.value) return
-    locale.value = l
+    const to = createLink(l, `${pageName.value}`)
+    return navigateTo(to)
   }
 
   const switchLocale = () => {
-    if(locale.value === 'ja'){
-      locale.value = 'en'
-    } else {
-      locale.value = 'ja'
-    }
-    const to = createLink(locale.value, `/${pageName.value}`)
+    const counterLocale = locale.value === 'ja'? 'en': 'ja'
+    const to = createLink(counterLocale, `/${pageName.value}`)
     return navigateTo(to)
   }
 
   const pageContentData = computed(() => {
-    const eachLocaleData = localeData[localeFromRoute.value]
+    const eachLocaleData = localeData[locale.value]
     return eachLocaleData[pageName.value as keyof typeof eachLocaleData]
   })
   const $t = (key: string) => pageContentData.value[key as keyof typeof pageContentData.value]
 
   return {
+    pageName,
     locale,
     setLocale,
     switchLocale,
